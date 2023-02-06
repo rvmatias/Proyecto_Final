df93421410847fe2639601a46f2de98eed03a313
rvmatias <ruizmatias.99@gmail.com>
2023-02-06 14:54:55 -0300

    se modifico el elemento card - agregro imagen

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,12 +1,13 @@
 
 import { Link } from "react-router-dom";
+import cataneo from "../Img/cataneo.jpg"
 
 function Card() {
     return (
         <>
         <Link to="/event">
         <div className="card text-bg-dark">
-            <img src= "" className="card-img" alt="..." />
+            <img src= {cataneo} className="card-img" alt="..." />
             <div className="card-img-overlay">
             </div>
         </div>
